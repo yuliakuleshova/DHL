6412af49cd2fb777930f4ebec2bfc55b9673394e
Iuliia Kuleshova <iuliia_kuleshova@epam.com>
2023-11-04 09:25:42 +0400

    RSS project

--- rss_reader.py
+++ rss_reader.py
@@ -16,16 +16,16 @@ cur = con.cursor()
 answer = requests.get(url).text
 all_news = []
 
-def create_result_data(date: list):
+def create_result_data(data: list):
     """
-    :param date:
+    :param data:
     :return: write search results to the file
     """
-    HEADER = '<!DOCTYPE html><html lang="RU"><head><title>All newses</title></head><body><ul id="navigation">'
+    HEADER = '<!DOCTYPE html><html lang="RU"><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><head><title>All newses</title></head><body><ul id="navigation">'
     BODY = ''
     FOOTER = '</ul></body></html>'
 
-    for item in date:
+    for item in data:
         BODY = BODY+f'<li><a href="{item['link']}">{item['title']}</a></li>'
     return HEADER+BODY+FOOTER
 
@@ -46,14 +46,13 @@ def convert_to_unixtime(datatime: str):
     :param datatime: input string with data and time
     :return: unix timestamp
     """
-    mnth = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Sep': 9, 'Oct': 10}
+    mnth = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12} #TODO add months
     dt = datatime.split(" ")
     tm = dt[4].split(":")
     tz1 = pytz.timezone('Europe/Moscow')
     tz2 = pytz.timezone('UTC')
     new_dt = datetime(int(dt[3]), int(mnth[dt[2]]), int(dt[1]), int(tm[0]), int(tm[1]), int(tm[2]))
     dt = tz1.localize(new_dt)
-    #print(dt, dt.astimezone(tz2))
     return time.mktime(dt.astimezone(tz2).timetuple())
 
 
@@ -76,6 +75,11 @@ def create_result_file(date: list):
     :param date:
     :return: write search results to the file
     """
+    f = open("news.html", "w", encoding="utf-8")
+    for i in range(len(date)):
+        f.write(str(date[i]))
+    f.close()
+
 
 
 def get_news_from_date(date: str):
@@ -98,15 +102,12 @@ for i in range(len(answer.split("\n"))):
         news['date'] = convert_to_unixtime(answer.split("\n")[i + 3].replace("<pubDate>", "").replace("</pubDate>", "").replace("    ", ""))
         news['decription'] = answer.split("\n")[i + 4].replace("<description>", "").replace("</description>",
                                                                                             "").replace("    ", "")
-        #print(news['link'])
+
         query = cur.execute("SELECT 1 link FROM rss WHERE link=?", [news['link']]).fetchone()
         if not query:
             cur.execute("INSERT INTO rss VALUES (?, ?, ?, ?)",
                         (news['link'], news['title'], news['date'], news['decription']))
-            #print('Added to DB')
             con.commit()
-        #else:
-            #print('Found in DB')
         all_news.append(news)
 
 #print(str(all_news).replace('\'', '"'))
@@ -119,7 +120,8 @@ for i in range(len(answer.split("\n"))):
 # data_yaml['RSS'] = news_json
 # print(yaml.dump(data_yaml, allow_unicode=True))
 
-print(create_result_data(all_news))
+#print(create_result_data(all_news))
+create_result_file(create_result_data(all_news))
 # with open('template.j2', 'r', encoding='utf-8') as j2:
 #     html_text = jinja2.Template(j2)
 #
